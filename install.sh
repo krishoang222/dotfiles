0dbeaa25e1d42668661b80b348e094c81ef5743f
krishoang <kristinker.work@gmail.com>
2025-08-17 23:51:17 +0700

    Added: Sketchybar add media playing in near-middle of bar

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ brew install --cask sf-symbols
 brew install --cask font-sf-mono
 brew install --cask font-sf-pro
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+brew install jq
