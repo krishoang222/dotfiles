b3ae159c69994ac1be277eb54fe730452b678303
TH <74447462+krishoang222@users.noreply.github.com>
2025-09-01 15:29:43 +0700

    Updated: smaller font size

--- .config/sketchybar/items/calendar.sh
+++ .config/sketchybar/items/calendar.sh
@@ -3,4 +3,6 @@
 sketchybar --add item calendar right \
            --set calendar icon=􀧞  \
                           update_freq=30 \
+                          icon.drawing=off \
+                          label.padding_left=10 \
                           script="$PLUGIN_DIR/calendar.sh"
--- .config/sketchybar/items/input_source.sh
+++ .config/sketchybar/items/input_source.sh
@@ -3,8 +3,10 @@
 sketchybar --add event input_change 'AppleSelectedInputSourcesChangedNotification' \
            --add item input_source right \
            --set input_source script="$PLUGIN_DIR/input_source.sh" \
+		 icon.font.size=15 \
 		 icon.padding_left=10 \
 		 icon.padding_right=0 \
 	         label.padding_left=0 \
 	         label.padding_right=8 \
+	         icon.y_offset=1 \
            --subscribe input_source input_change
